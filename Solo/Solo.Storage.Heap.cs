5a47f7372f7830dcf60a20c81237ef32b508f01b
VitalySokoloff <vitaliysokoloff@gmail.com>
2023-02-15 23:16:33 +0700

    0.2.0.1

---
+++ Solo/Solo.Storage.Heap.cs
@@ -0,0 +1,192 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using System.Text;
+
+namespace Solo.Storage
+{
+    public class Heap
+    {
+        private Dictionary<string, int> _ints;
+        private Dictionary<string, float> _floats;
+        private Dictionary<string, string> _strings;
+        private Dictionary<string, bool> _bools;
+        private Dictionary<string, Point> _points;
+        private Dictionary<string, Vector2> _vectors;
+        private Dictionary<string, Heap> _heaps;
+
+        public Heap()
+        {
+            _ints = new Dictionary<string, int>();
+            _floats = new Dictionary<string, float>();
+            _strings = new Dictionary<string, string>();
+            _bools = new Dictionary<string, bool>();
+            _points = new Dictionary<string, Point>();
+            _vectors = new Dictionary<string, Vector2>();
+            _heaps = new Dictionary<string, Heap>();
+        }
+
+        public void Add(string key, int value)
+        {
+            _ints.Add(key, value);
+        }
+
+        public void Add(string key, float value)
+        {
+            _floats.Add(key, value);
+        }
+
+        public void Add(string key, string value)
+        {
+            _strings.Add(key, value);
+        }
+
+        public void Add(string key, bool value)
+        {
+            _bools.Add(key, value);
+        }
+
+        public void Add(string key, Point value)
+        {
+            _points.Add(key, value);
+        }
+
+        public void Add(string key, Vector2 value)
+        {
+            _vectors.Add(key, value);
+        }
+
+        public void Add(string key, Heap value)
+        {
+            _heaps.Add(key, value);
+        }
+
+        public int GetInt(string key)
+        {
+            if (_ints.ContainsKey(key))
+            {
+                return _ints[key];
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public float GetFloat(string key)
+        {
+            if (_floats.ContainsKey(key))
+            {
+                return _floats[key];
+            }
+            else
+            {
+                return 0f
+;
+            }
+        }
+
+        public string GetString(string key)
+        {
+            if (_strings.ContainsKey(key))
+            {
+                return _strings[key];
+            }
+            else
+            {
+                return "DON'T EXIST";
+            }
+        }
+
+        public bool GetBool(string key)
+        {
+            if (_bools.ContainsKey(key))
+            {
+                return _bools[key];
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Point GetPoint(string key)
+        {
+            if (_points.ContainsKey(key))
+            {
+                return _points[key];
+            }
+            else
+            {
+                return Point.Zero;
+            }
+        }
+
+        public Vector2 GetVector2(string key)
+        {
+            if (_vectors.ContainsKey(key))
+            {
+                return _vectors[key];
+            }
+            else
+            {
+                return Vector2.Zero;
+            }
+        }
+
+        public Heap GetHeap(string key)
+        {
+            if (_heaps.ContainsKey(key))
+            {
+                return _heaps[key];
+            }
+            else
+            {
+                return new Heap();
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (string key in _ints.Keys)
+            {
+                sb.Append("int\t" + key + ": " + _ints[key] + "\n");
+            }
+
+            foreach (string key in _floats.Keys)
+            {
+                sb.Append("float\t" + key + ": " + _floats[key] + "\n");
+            }
+
+            foreach (string key in _strings.Keys)
+            {
+                sb.Append("string\t" + key + ": " + _strings[key] + "\n");
+            }
+
+            foreach (string key in _bools.Keys)
+            {
+                sb.Append("bool\t" + key + ": " + _bools[key] + "\n");
+            }
+
+            foreach (string key in _points.Keys)
+            {
+                sb.Append("point\t" + key + ": " + _points[key].ToString() + "\n");
+            }
+
+            foreach (string key in _vectors.Keys)
+            {
+                sb.Append("vector2\t" + key + ": " + _vectors[key].ToString() + "\n");
+            }
+
+            foreach (string key in _heaps.Keys)
+            {
+                sb.Append("\nheap\t" + key + "{\n");
+                sb.Append(_heaps[key].ToString());
+                sb.Append("}\n");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Solo/Solo.Storage.IO.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.Xna.Framework;
+
+namespace Solo.Storage
+{
+    public static class IO
+    {
+        static private string _intPattern = @"^.+\s*:\s*\d+\s*$";
+        static private string _floatPattern = @"^.+\s*:\s*\d+,\d+\s*$";
+        static private string _stringPattern = "^.+\\s*:\\s*\".+\"\\s*$";
+        static private string _heapPattern = @"^.+\s*{\s*$";
+        static private string _endPattern = @"^\s*}\s*$";
+        static private string _boolPattern = @"^.+\s*:\s*\+|true|True|on|On|-|false|False|off|Off\s*$";
+        static private string _pointPattern = @"^.+\s*:\s*\d\.\d+\s*$";
+        static private string _vectorPattern = @"^.+\s*:\s*\d+,\d+\.\d+,\d+\s*$";
+
+        public static Heap ReadHeap(string path)
+        {
+            Heap heap = new Heap();
+            string fileName = path + ".heap";
+
+            if (!File.Exists(fileName))
+            {
+                File.Create(fileName);
+                return heap;
+            }
+
+            using (StreamReader sr = new StreamReader(fileName))
+            {               
+                heap = FillHeap(sr);
+            }
+
+            return heap;
+        }
+
+        private static Heap FillHeap(StreamReader sr)
+        {
+            Heap heap = new Heap();
+
+            while (sr.Peek() >= 0)
+            {
+                string[] data = ParseString(sr.ReadLine());
+
+                switch (data[0])
+                {
+                    case "int":
+                        heap.Add(data[1], Convert.ToInt32(data[2]));
+                        break;
+                    case "float":
+                        heap.Add(data[1], (float)Convert.ToDouble(data[2]));
+                        break;
+                    case "string":
+                        heap.Add(data[1], data[2]);
+                        break;
+                    case "bool":
+                        heap.Add(data[1], data[2] == "+" || data[2] == "true" || data[2] == "True" || data[2] == "on" || data[2] == "On" ? true : false);
+                        break;
+                    case "point":
+                        heap.Add(data[1], new Point(Convert.ToInt32(data[2]), Convert.ToInt32(data[3])));
+                        break;
+                    case "vector2":
+                        heap.Add(data[1], new Vector2((float)Convert.ToDouble(data[2]), (float)Convert.ToDouble(data[3])));
+                        break;
+                    case "heap":
+                        heap.Add(data[1], FillHeap(sr));                        
+                        break;
+                    case "end":
+                        return heap;
+                    default:
+                        break;
+                }
+            }
+            return heap;
+        }
+
+        public static bool SaveHeap(Heap heap) { return true; }
+
+        static private string[] ParseString(string str)
+        {
+            string[] answer;
+
+            str = str.Trim(' ');
+
+            if (Regex.IsMatch(str, _intPattern))
+            {
+                string[] tmp = str.Split(':');
+
+                answer = new string[3];
+
+                answer[0] = "int";
+                answer[1] = tmp[0].TrimEnd(' ');
+                answer[2] = tmp[1].TrimStart(' ');
+
+                return answer;    
+            }
+
+            if (Regex.IsMatch(str, _floatPattern))
+            {
+                string[] tmp = str.Split(':');
+
+                answer = new string[3];
+
+                answer[0] = "float";
+                answer[1] = tmp[0].TrimEnd(' ');
+                answer[2] = tmp[1].TrimStart(' ');
+
+                return answer;
+            }
+
+            if (Regex.IsMatch(str, _stringPattern))
+            {
+                string[] tmp = str.Split(':');
+
+                answer = new string[3];
+
+                answer[0] = "string";
+                answer[1] = tmp[0].TrimEnd(' ');
+                answer[2] = tmp[1].TrimStart(' ').Trim('"');
+
+                return answer;
+            }
+
+            if (Regex.IsMatch(str, _boolPattern))
+            {
+                string[] tmp = str.Split(':');
+
+                answer = new string[3];
+
+                answer[0] = "bool";
+                answer[1] = tmp[0].TrimEnd(' ');
+                answer[2] = tmp[1].TrimStart(' ');
+
+                return answer;
+            }
+
+            if (Regex.IsMatch(str, _pointPattern))
+            {
+                string[] tmp = str.Split(':');
+
+                answer = new string[4];
+
+                answer[0] = "point";
+                answer[1] = tmp[0].TrimEnd(' ');
+
+                string[] tmp2 = tmp[1].TrimStart(' ').Split('.');
+
+                answer[2] = tmp2[0];
+                answer[3] = tmp2[1];
+
+                return answer;
+            }
+
+            if (Regex.IsMatch(str, _vectorPattern))
+            {
+                string[] tmp = str.Split(':');
+
+                answer = new string[4];
+
+                answer[0] = "vector2";
+                answer[1] = tmp[0].TrimEnd(' ');
+
+                string[] tmp2 = tmp[1].TrimStart(' ').Split('.');
+
+                answer[2] = tmp2[0];
+                answer[3] = tmp2[1];
+
+                return answer;
+            }
+
+            if (Regex.IsMatch(str, _heapPattern))
+            {
+                answer = new string[2];
+
+                answer[0] = "heap";
+                answer[1] = str.Trim(' ').TrimEnd('{');
+
+                return answer;
+            }
+
+            if (Regex.IsMatch(str, _endPattern))
+            {
+                answer = new string[1];
+
+                answer[0] = "end";
+
+                return answer;
+            }
+
+            answer = new string[3];
+            return answer;
+        }
+    }
+}
